247b536cef26ae529e4f5a49b49f28546a7df015
mkhaque1 <mkhaque29@gmail.com>
2026-07-06 20:35:30 +0200

    switch to access and refresh token pattern

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,17 +1,29 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
+import config from '../config';
 
 export interface JwtPayload {
   id: string;
   role: string;
 }
 
-export const signToken = (payload: JwtPayload): string => {
+export const signAccessToken = (payload: JwtPayload): string => {
   const options: SignOptions = {
-    expiresIn: process.env.jwt_access_expires_in as SignOptions['expiresIn'],
+    expiresIn: config.jwt_access_expires_in as SignOptions['expiresIn'],
   };
-  return jwt.sign(payload, process.env.JWT_SECRET as string, options);
+  return jwt.sign(payload, config.jwt_access_secret, options);
 };
 
-export const verifyToken = (token: string): JwtPayload => {
-  return jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+export const signRefreshToken = (payload: JwtPayload): string => {
+  const options: SignOptions = {
+    expiresIn: config.jwt_refresh_expires_in as SignOptions['expiresIn'],
+  };
+  return jwt.sign(payload, config.jwt_refresh_secret, options);
+};
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+  return jwt.verify(token, config.jwt_access_secret) as JwtPayload;
+};
+
+export const verifyRefreshToken = (token: string): JwtPayload => {
+  return jwt.verify(token, config.jwt_refresh_secret) as JwtPayload;
 };
