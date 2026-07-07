c827b018f7ef748c0dce09a0b1ea5e4ad5b64e84
mkhaque1 <mkhaque29@gmail.com>
2026-07-07 11:05:06 +0200

    add public property routes

--- src/module/property/property.routes.ts
+++ src/module/property/property.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { PropertyControllers } from './property.controller';
 
 const router = Router();
+
+router.get('/', PropertyControllers.getAllProperties);
+router.get('/:id', PropertyControllers.getPropertyById);
 
 export const PropertyRoutes = router;
