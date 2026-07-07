23db6a6781a81aa35cfc2905b4da856a36bd53fe
mkhaque1 <mkhaque29@gmail.com>
2026-07-07 15:17:50 +0200

    added rental routes

--- src/module/rental/rental.routes.ts
+++ src/module/rental/rental.routes.ts
@@ -1,5 +1,25 @@
 import { Router } from 'express';
+import { RentalControllers } from './rental.controller';
+import { authenticate, restrictTo } from '../../middlewares/auth';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { createRentalRequestValidationSchema } from './rental.validation';
 
 const router = Router();
+
+router.post(
+  '/',
+  authenticate,
+  restrictTo('TENANT'),
+  validateRequest(createRentalRequestValidationSchema),
+  RentalControllers.createRentalRequest,
+);
+
+router.get(
+  '/',
+  authenticate,
+  restrictTo('TENANT'),
+  RentalControllers.getMyRentalRequests,
+);
+router.get('/:id', authenticate, RentalControllers.getRentalRequestById);
 
 export const RentalRoutes = router;
